56239fc08993cc414de4965ea82f37cdb369fffb
Sai Mithun Nakka <sai.mithun.72@gmail.com>
2014-08-26 10:05:54 -0400

    added a little helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Return a title on a par page
+  def title
+    base_title = "Ruby on rails helper"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+
 end
